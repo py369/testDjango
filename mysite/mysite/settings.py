62e2455eb08533b7db02fa65673117dee873e308
adele <nieuweb369@gmail.com>
2024-12-18 10:04:10 +0100

    opmerking over taal in settings.py toegevoegd

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -103,6 +103,7 @@ AUTH_PASSWORD_VALIDATORS = [
 
 # Internationalization
 # https://docs.djangoproject.com/en/5.1/topics/i18n/
+# hieronder bepaalt u de taal van uw applicatie
 
 LANGUAGE_CODE = 'en-us'
 
