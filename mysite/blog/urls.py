33d681cb0f4219b47d71bf829d417e141ecf39df
adele <adelealavee@gmail.com>
2025-01-20 14:03:56 +0100

    update posts,make post, make delete option,install polls, change bg polls to python

--- mysite/blog/urls.py
+++ mysite/blog/urls.py
@@ -3,4 +3,7 @@ from . import views
 
 urlpatterns = [ 
                path('', views.post_list, name='post_list'),
+               path('post/<int:pk>/', views.post_detail, name='post_inhoud'),
+               path('post/nieuw/', views.post_nieuw, name='post_nieuw'),
+               path('delete/<post_id>',views.delete_post,name='delete')
                ]
--- mysite/mysite/urls.py
+++ mysite/mysite/urls.py
@@ -1,8 +1,10 @@
 
 from django.contrib import admin
 from django.urls import path, include
+from django.conf.urls import include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('blog.urls')),
+    path('polls/', include('polls.urls')),
 ]
